a266d1d50048a8d5a98cbc6dc287705382cd4493
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-14 21:24:43 +0530

    recursive

--- 03-recursion-Python/recursion.py
+++ 03-recursion-Python/recursion.py
@@ -4,4 +4,8 @@ Your code should have the same input/output as the
 iterative code in the instructions."""
 
 def get_fib(position):
-    return -1
+    l = [0,1]
+    for i in range(0,15,1):
+        j = l[i] + l[i+1]
+        l.append(j)
+    return l[position]
