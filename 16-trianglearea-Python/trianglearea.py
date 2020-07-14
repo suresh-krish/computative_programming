043fe7d49c1c92e447afebd3ce81fe5a07ef7e55
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-14 12:35:16 +0530

    Jul 14  12:35:16

--- 16-trianglearea-Python/trianglearea.py
+++ 16-trianglearea-Python/trianglearea.py
@@ -7,12 +7,11 @@ import math
 
 def trianglearea(s1, s2, s3):
 	# your code goes here
-	s5 = (s1 + s2 + s2)
-	s = s5 / 2
+	s = (s1 + s2 + s2) / 2.0
 	sum = 1
 	sum = s - s1
 	sum = sum * (s-s2)
 	sum = sum * (s-s3)
 	b = s * sum
 	c = math.sqrt(b)
-	return c
+	return s
