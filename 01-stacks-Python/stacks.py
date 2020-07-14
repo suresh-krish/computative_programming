632547391d418d59a3aedd0253609d06659e2a7c
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-14 21:10:47 +0530

    Jul 14  21:10:47

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -27,20 +27,17 @@ class LinkedList(object):
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
         # append(new_element)
-        current = self.head
-        if self.head:
-            while current.next:
-                current = current.next
-            current.next = new_element
-        else:
-            self.head = new_element
+        new_element.next = self.head
+        self.head = new_element
 
 
     def delete_first(self):
         "Delete the first (head) element in the LinkedList as return it"
-        res = head.next.value
-        top = head.next
-        self.head = top
+        if self.head == None:
+            return None
+        res = self.head
+        self.head = res.next
+        return res
 
 
 class stack(object):
@@ -49,11 +46,11 @@ class stack(object):
 
     def push(self, new_element):
         "Push (add) a new element onto the top of the stack"
-        LinkedList.insert_first(self,new_element)
+        self.ll.insert_first(new_element)
         
     def pop(self):
         "Pop (remove) the first element off the next of the stack and return it"
-        LinkedList.delete_first(self)
+        a = self.ll.delete_first()
         
 
 
