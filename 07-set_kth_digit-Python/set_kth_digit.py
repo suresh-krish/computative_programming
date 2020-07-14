3a848ae35d905474202c4e16111660502f589b56
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-14 23:45:05 +0530

    Jul 14  23:45:05

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,12 +7,22 @@
 
 
 def fun_set_kth_digit(n, k, d):
+	flag = 0
+	if n < 0:
+		flag = 1
 	l = str(n)
 	lis = list(l)
+	if len(lis)<=d:
+		lis.append(str(d))
+
 	list1 = lis[::-1]
 	list1[k] = str(d)
 	res = list1[::-1]
-	res1 = str(res)
+	res1 = ""
+	for i in res:
+		res1 = res1 + i
+	if flag == 1:
+		res1 = "-" + res1
 	return int(res1)
 
 
