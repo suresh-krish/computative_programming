1448d5a2e4fb19c2738728c902ac6e9598014120
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-14 17:13:13 +0530

    Jul 14  17:13:13

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -26,14 +26,14 @@ class LinkedList(object):
 
     def insert_first(self, new_element):
         "Insert new element as the head of the LinkedList"
-        append(new_element)
-        # current = self.head
-        # if self.head:
-        #     while current.next:
-        #         current = current.next
-        #     current.next = new_element
-        # else:
-        #     self.head = new_element
+        # append(new_element)
+        current = self.top
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
 
 
     def delete_first(self):
