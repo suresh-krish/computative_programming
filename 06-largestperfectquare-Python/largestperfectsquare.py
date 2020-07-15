4ff1ba680bd887d00316a62fae07f3a8b8aff47e
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-15 16:09:32 +0530

    Jul 15  16:09:32

--- 06-largestperfectquare-Python/largestperfectsquare.py
+++ 06-largestperfectquare-Python/largestperfectsquare.py
@@ -6,7 +6,10 @@
 # assert(largestPerfectSquare(26) == 25)
 # Hint: you may wish to use a similar approach to how you solved isPerfectSquare on the hw.
 # Another hint: This can be written using just one or two lines of Python.
-
+import math
 def largestperfectsquare(n):
 	# your code goes here
-	pass
+	for i in range(0,math.sqrt(n)+1,-1):
+		if i ** i < n:
+			if math.ceil(i**i) == i**i:
+				return i
