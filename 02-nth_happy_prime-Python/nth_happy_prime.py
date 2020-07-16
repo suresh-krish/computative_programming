682b4c4d58c6d86433d376ea1b615f23fbab6785
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-16 22:17:24 +0530

    nthhappyprime

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -12,26 +12,47 @@ def checkin(n):
   # print("sum",sum)
   return sum
 
+def is_prime(a):
+  print(a)
+  flag = 0
+  for i in range(2,a):
+    if a % i == 0:
+      flag = 1
+  if flag == 0:
+    print("isprime")
+    return 0
+  return 1
+    
+
 
 def fun_nth_happy_prime(n):
-	if n == 0:
-		return 1
-	nth = 2
-	res = 2
-	p = 0
-	while p!=n:
-    # print("p", p)
-    # printh("nth",nth)
-		while nth != 1 and nth !=4:
-			nth = checkin(nth)
-		if nth % 2 ==  0:
-			p = p + 1
-			res = res + 1
-			nth = res
-		else:
-		    res = res + 1
-		    nth = res
+  if n == 0:
+    return 7
+  else:
+    nth = 8
+    res = 8
+    p = 0
+    while p!=n:
+      print("p", p)
+      # print(res)
+      # printh("nth",nth)
+      while nth != 1 and nth !=4:
+        nth = checkin(nth)
+      if nth  ==  1:
+        print(res,"res")
+        z = is_prime(res)
+        print("z",z)
+        if z !=0 :
+          # p = p + 1
+          res = res + 1
+          nth = res
+        else:
+          p = p + 1
+          res = res + 1
+          nth = res
+        
+      else:
+          res = res + 1
+          nth = res
 
-	return nth - 1
-
-	return 0
+    return res - 1
