913a9fa528262d12967dcf505aec791829b63826
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-18 18:24:09 +0530

    Jul 18  18:24:09

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -26,8 +26,15 @@ class BST(object):
         self.placing(self.root,new_val)
 
     def printSelf(self):
-        # Your code goes here
-        pass
+        if self.root:
+            self.traverseInOrder(self.root)
+            
+    def traverseInOrder(self, node):
+        if node.left:
+            self.traverseInOrder(node.left)
+            return node.value
+        if node.right:
+            self.traverseInOrder(node.right)
 
     def searching(self,root,find_val):
         if root.value == find_val:
@@ -49,7 +56,7 @@ class BST(object):
     def search(self, find_val):
         # Your code goes here
         if not self.root:
-            return None
+            return False
         elif type(find_val) != int:
             return False
         else :
