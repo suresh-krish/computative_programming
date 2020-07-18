d12d3177553c25600f7bc13b3d8f0f7f1d599876
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-18 13:03:26 +0530

    Jul 18  13:03:25

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -5,13 +5,20 @@ Output a sorted list."""
 def sortin(array,low,high):
 	i = low
 	j = high
-	while i <= j:
+	while i < j:
 		if array[i] < array[j]:
 			b = array[low]
 			array[low] = array[i]
 			array[i] = b
 			i = i + 1
 			low = low + 1
+		else:
+			i = i + 1
+
+	b = array[low]
+	array[low] = array[j]
+	array[j] = b
+	return array,low + 1,high
 
 
 
@@ -20,4 +27,7 @@ def quicksort(array):
 	low = 0
 	high = len(array) - 1
 	pivot = high
-	low,high = sortin(array,low,high)
+	while low < high:
+	    low,high = sortin(array,low,high)
+
+	return array
