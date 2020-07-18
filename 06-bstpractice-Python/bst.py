1be8bf893ddc4cf58655300605b24d00a05c62b2
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-18 17:56:48 +0530

    Jul 18  17:56:48

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -8,15 +8,53 @@ class BST(object):
     def __init__(self, root):
         self.root = Node(root)
 
+    def placing(self,root,new_val):
+        if self.root.value > new_val:
+            if self.root.left != None:
+                placing(self.root.left,new_val)
+            else :
+                self.root.left = new_val
+        elif self.root.value < new_val:
+            if self.root.right != None:
+                placing(self.root.right,new_val)
+            else :
+                self.root.right = new_val
+        
+
     def insert(self, new_val):
         # Your code goes here
-        pass
+        if not self.root:
+            self.root = new_val
+        else:
+           placing(self.root,new_val)
 
     def printSelf(self):
         # Your code goes here
         pass
+
+    def searching(self,root,new_val):
+        if self.root.value == new_val:
+            return True
+
+
+        elif self.root.value > new_val:
+            if self.root.left != None:
+                searching(self.root.left,new_val)
+            else:
+                return False
+        elif self.root.value < new_val:
+            if self.root.right != None:
+                searching(self.root.right,new_val)
+            else:
+                return False
+
+
+
         
     def search(self, find_val):
         # Your code goes here
-        pass
+        if not self.root:
+            return None
+        else :
+            searching(self.root, find_val)
 
