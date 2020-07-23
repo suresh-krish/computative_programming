86a42d8ba3412599cb459d61a3cf759853d09a79
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-23 18:53:51 +0530

    Jul 23  18:53:51

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -8,24 +8,51 @@
 # so fun_nthsmithnumber(1) should return 22
 
 
-def isprime(i):
+def isprime(n):
+    i = 2
     lis = []
-    while i % 2 == 0:
-        lis.append(2)
-        i = i / 2
+    while i*i <= n:
+        if n % i:
+            i += 1
+        else :
+            n //= i
+            lis.append(i)
+    if n > 1:
+        lis.append(n)
+
+    return lis
+
 
 
 
 
 def fun_nth_smithnumber(n):
     x = 4
+    p = 0
     if n == 0:
         return 4
 
 
     while p != n:
         x = x + 1
-        p = 0
         l = isprime(x)
+
+        sum = 0
+        for i in l:
+            while i != 0:
+                p = i % 10
+                sum = sum + p
+                i = i // 10
+
+        res = 0
+        ch = x
+        while x != 0:
+            p = x % 10
+            res = res + p
+            ch = ch // 10
+
+        if res == sum:
+            p =p + 1
+
 
     return x
