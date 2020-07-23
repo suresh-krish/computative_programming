08028a6e8853cf54bc8c9267374c7a4e28ffd19c
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-23 20:10:22 +0530

    Jul 23  20:10:22

--- 05-nthwithproperty309-Python/nthwithproperty309.py
+++ 05-nthwithproperty309-Python/nthwithproperty309.py
@@ -5,5 +5,33 @@
 # the nth number with Property309.
 
 def nthwithproperty309(n):
-	# Your code goes here
-	pass
+	a = 309
+	p = 0
+	ch = "0123456789"
+	if n == 0:
+		return 309
+
+	while p != n:
+		flag = 0
+		a = a + 1
+		j = a
+		for i in range(0,5):
+			j = j * a
+
+		j = list(str(a))
+		z = set(j)
+		for i in ch:
+			if i not in z:
+				flag = 1
+				break
+
+		if flag == 0:
+			p = p + 1
+
+	return a
+
+
+
+
+
+	
