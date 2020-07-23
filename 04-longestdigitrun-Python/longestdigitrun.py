898882dd4b979091d7606cc1c1435c54e3242188
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-23 19:59:26 +0530

    Jul 23  19:59:26

--- 04-longestdigitrun-Python/longestdigitrun.py
+++ 04-longestdigitrun-Python/longestdigitrun.py
@@ -9,9 +9,9 @@ def longestdigitrun(n):
 		n = abs(n)
 
 	n = str(n)
-	l = [(0,0)]
+	l = [(0,0),]
 	for i in n:
-		if i in l[-1][0]:
+		if int(i) in l[-1][0]:
 			l.append((i,l[-1][1] + 1))
 		else:
 			l.append((i,1))
