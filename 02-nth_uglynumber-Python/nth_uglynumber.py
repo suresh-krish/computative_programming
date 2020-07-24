8d49fbecde3ccd72684c5b4106e800a3cfed253d
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-24 17:49:55 +0530

    Jul 24  17:49:54

--- 02-nth_uglynumber-Python/nth_uglynumber.py
+++ 02-nth_uglynumber-Python/nth_uglynumber.py
@@ -22,13 +22,15 @@ def primefac(a):
 def fun_nth_uglynumber(n):
     x = 6
     p = 5
+    num = 1
     if n == 0 or n == 1 or n == 2 or n == 3 or n == 4 or n == 5:
         return n + 1
 
     while p != n:
         x = x + 1
+        num = x
         sum = 1
-        l = primefac(x)
+        l = primefac(num)
         if 2 in l or 3 in l or 5 in l:
             for i in l:
                 if i > 5:
@@ -39,4 +41,4 @@ def fun_nth_uglynumber(n):
             if sum == x:
                 p = p + 1
 
-    return x
+    return num
