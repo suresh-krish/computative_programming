ad5ca9ddf66aa956adf72f538bb9f4f298b5234a
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-24 12:58:57 +0530

    Jul 24  12:58:57

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -5,21 +5,6 @@
 # For example:- 36 is a powerful number. It is divisible by both 3 and square of 3 i.e, 9.
 import math
 
-def primefactors(a):
-	li = []
-	while a % 2 == 0:
-		li.append(2)
-		a = a / 2
-
-	for i in range(3,int(math.sqrt(a)) + 1,2):
-		while a % i == 0:
-			li.append(i)
-			a = a / i
-
-	return li
-		
-
-
 def nthpowerfulnumber(n):
 	s = 0
 	z = 3
@@ -29,18 +14,28 @@ def nthpowerfulnumber(n):
 	
 	while s != n:
 		z = z + 1
-		l = primefactors(z)
-		print("--------------")
-		print("s",s)
-		print("z",z)
-		print(l)
-		temp = 0
-		for i in l:
-			if z % i == 0 and z % (i ** 2) == 0:
-				temp = 1
+		while z % 2 == 0:
+			po = 0
+			while z % 2 == 0:
+				z = z // 2
+				po = po + 1
 
-		if temp == 1:
-			s = s + 1
-				
+			if po == 1:
+				z = z + 1
+				break
+			else:
+				s = s + 1
+		
+		for i in range(3,int(math.sqrt(z))+1,2):
+			po = 0
+			while (z % i == 0):
+				z = z // i
+				po = po + 1
+
+			if po == 1:
+				z = z + 1
+				break
+			else:
+				s = s + 1
 
 	return z
