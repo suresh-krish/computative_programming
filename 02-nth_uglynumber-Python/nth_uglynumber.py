3d3578a3c86979bf389a42a26054c0ccf13dc719
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-24 16:32:48 +0530

    Jul 24  16:32:48

--- 02-nth_uglynumber-Python/nth_uglynumber.py
+++ 02-nth_uglynumber-Python/nth_uglynumber.py
@@ -1,7 +1,28 @@
 # Write the function nthUglyNumber that takes a non-negative int n and returns the nth Ugly Number. 
 # Ugly numbers are numbers whose only prime factors are 2, 3 or 5. The sequence 1, 2, 3, 4, 5, 6, 8, 
 # 9, 10, 12, 15 shows the few ugly numbers. By convention, nthUglyNumber(0) will give 1.
+import math
+
+
+def primefac(a):
+    li = []
+    while a % 2 == 0:
+        li.append(2)
+        a = a // 2
+
+    for i in range(3,int(math.sqrt(a) + 1),2):
+        while a % i == 0:
+            li.append(i)
+            a =a // i
+
 
 
 def fun_nth_uglynumber(n):
-    return 0
+    x = 1
+    p = 0
+    if n == 0:
+        return 1
+
+    while p != n:
+        x = x + 1
+        l = primefac(x)
