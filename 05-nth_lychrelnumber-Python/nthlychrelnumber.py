0dc2e12981febca4dd0beea1fe0add465ddc26d2
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-24 21:52:23 +0530

    Jul 24  21:52:23

--- 05-nth_lychrelnumber-Python/nthlychrelnumber.py
+++ 05-nth_lychrelnumber-Python/nthlychrelnumber.py
@@ -5,6 +5,38 @@
 # The first few Lychrel numbers are 196, 295, 394, 493, 592, 689, 691, 788, 790, 879, 887….
 
 
+def palindrom(i):
+	if i == rotate(i):
+		return True
+
+	return False
+
+def rotate(a):
+	sum = 0
+	while a!=0:
+		k = a % 10
+		sum = (sum * 10) + k
+		a = a // 10
+
+	return sum
+
+
 def nthlychrelnumbers(n):
-	# your code goes here
-	pass
+	maxi = 25
+	x = 195
+	p = 0
+
+	while p != n:
+		x = x + 1
+		num = x
+		flag = 0
+		for i in range(maxi):
+			num  = num + rotate(num)
+			if palindrom(num):
+				flag = 1
+				break
+
+		if flag == 0:
+			p = p + 1
+
+	return x
