5a5d55df6adc420d9b1135eca58b86f617fc71db
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-25 18:50:36 +0530

    Jul 25  18:50:35

--- 06-leastfrequentletters-Python/leastfrequentletters.py
+++ 06-leastfrequentletters-Python/leastfrequentletters.py
@@ -8,5 +8,16 @@
 # if s does not contain any alphabetic characters, the result should be the empty string ("")
 
 def leastfrequentletters(s):
-	# Your code goes here
-	pass
+	s = s.lowercase()
+	d = {}
+	for i in s:
+		if i in d:
+			d[i] = d[i] + 1
+		else:
+			d[i] = 1
+
+	res = ""
+	for i in range(1,len(s)):
+		for j in d:
+			print(d)
+
