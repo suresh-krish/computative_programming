4bdd550740b18478140ac0995b477123b4f2b8c1
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-25 19:06:51 +0530

    Jul 25  19:06:51

--- 06-leastfrequentletters-Python/leastfrequentletters.py
+++ 06-leastfrequentletters-Python/leastfrequentletters.py
@@ -8,18 +8,19 @@
 # if s does not contain any alphabetic characters, the result should be the empty string ("")
 
 def leastfrequentletters(s):
-	# if s == "":
-		# return ""
+	a = "abcdefghijklmnopqrstuvwxyz"
+	if s == "":
+		return ""
 
 
 	s = s.lower()
 	print(s)
 	d = {}
 	for i in s:
-		if i in d:
-			d[i] = d[i] + 1
+		if i in a and i not in d:
+			d[i] =  1
 		else:
-			d[i] = 1
+			d[i] = d[i] + 1
 
 	res = ""
 	for i in range(1,len(s)):
