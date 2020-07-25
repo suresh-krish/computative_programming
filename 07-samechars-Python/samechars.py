3cf2bbce77561255e22e98d7008f0d44a9560aaf
suresh-krish <sureshvinz66@msitprogram.net>
2020-07-25 19:25:57 +0530

    samechars

--- 07-samechars-Python/samechars.py
+++ 07-samechars-Python/samechars.py
@@ -5,6 +5,28 @@
 # case-sensitive, so "ABC" and "abc" do not contain the same characters. The function returns False if either 
 # parameter is not a string, but returns True if both strings are empty (why?).
 
-def samechars(s1, s2):
-	# Your code goes here
-	pass
+def samechars(s):
+	s1 = s[0]
+	s2 = s[1]
+	if s1 == "" and s2 == "":
+		return True
+
+	elif type(s1) != str or type(s2) != str:
+		return False
+
+	elif s1 == "" or s2 == "":
+		return False
+
+	else:
+		if len(s1) <= len(s2):
+			for i in s1:
+				if i not in s2:
+					return False
+			return True
+
+		else:
+			for i in s2:
+				if i not in s1:
+					return False
+			return True
+
